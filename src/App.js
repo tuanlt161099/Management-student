693616356966b196128bef5d1dd2b872a3a6bdae
Tuanlt161099 <tuanlt161099@gmail.com>
2021-04-20 17:45:08 +0700

    update formInfor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FormInfor from './components/FormInfor'
 function App() {
   return (
     <>
-
     <FormInfor/>
   </>
   )
--- src/components/FormInfor.js
+++ src/components/FormInfor.js
@@ -1,79 +1,150 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function FormInfor(props) {
+  const initializa = {
+    name: "",
+    studentCode: "",
+    major: "",
+    address: "",
+    phone: null,
+  };
+
+  
+  const [infor, setInfor] = useState({
+    name: "1",
+    studentCode: "1",
+    major: "1",
+    address: "1",
+    phone: null,
+  });
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    console.log(e)
+
+
+    e.preventDefault();
+    const id = Math.floor(Math.random() * 10000);
+
+    const newInfor = {id, ...infor};
+    setInfor(newInfor);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.name)
+    const infor2 = {...infor, [e.target.name] : e.target.value}
+    setInfor(infor2);
+  };
+
+  console.log(infor);
+
   return (
     <>
       <button
         type="button"
-        class="btn btn-success"
+        className="btn btn-success"
         data-toggle="modal"
         data-target="#exampleModalCenter"
       >
-        Add information Student <i class="fas fa-plus"></i>
+        Add Information Student <i className="fas fa-plus"></i>
       </button>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="exampleModalCenter"
         tabindex="-1"
         role="dialog"
         aria-labelledby="exampleModalCenterTitle"
         aria-hidden="true"
       >
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">
-                Modal title
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">
+                Input Information Form
               </h5>
               <button
                 type="button"
-                class="close"
+                className="close"
                 data-dismiss="modal"
                 aria-label="Close"
               >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-4">.col-md-4</div>
-                  <div class="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
-                </div>
-                <div class="row">
-                  <div class="col-md-3 ml-auto">.col-md-3 .ml-auto</div>
-                  <div class="col-md-2 ml-auto">.col-md-2 .ml-auto</div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6 ml-auto">.col-md-6 .ml-auto</div>
-                </div>
-                <div class="row">
-                  <div class="col-sm-9">
-                    Level 1: .col-sm-9
-                    <div class="row">
-                      <div class="col-8 col-sm-6">
-                        Level 2: .col-8 .col-sm-6
-                      </div>
-                      <div class="col-4 col-sm-6">
-                        Level 2: .col-4 .col-sm-6
-                      </div>
+            <div className="modal-body">
+              <div className="container-fluid">
+                <form onSubmit={handleSubmit}>
+                  <div className="form-row">
+                    <div className="form-group col-md-6">
+                      <label for="name">Full Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        placeholder="Nguyễn Văn A"
+                        value={infor.name}
+                        onChange={handleChange}
+                        //ref={inputRef}
+                      />
+                    </div>
+                    <div className="form-group col-md-6">
+                      <label for="id">Student Code</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="studentCode"
+                        placeholder="K43.104.xxx"
+                        value={infor.id}
+                        onChange={handleChange}
+                      />
                     </div>
                   </div>
-                </div>
+                  <div className="form-group">
+                    <label for="major">Major</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="major"
+                      placeholder="Công nghệ thông tin..."
+                      value={infor.major}
+                      onChange={handleChange}
+                      ref={inputRef}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputAddress">Address</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="address"
+                      placeholder="42/1 Ung Văn Khiêm..."
+                      value={infor.address}
+                      onChange={handleChange}
+                      ref={inputRef}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputPhone">Phone Number</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="phone"
+                      placeholder="099405xxx"
+                      value={infor.phone}
+                      onChange={handleChange}
+                      ref={inputRef}
+                    />
+                  </div>
+
+                  <button className="btn btn-primary" type='submit'>ADD</button>
+                </form>
               </div>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" class="btn btn-primary">
-                Save changes
-              </button>
             </div>
           </div>
         </div>
